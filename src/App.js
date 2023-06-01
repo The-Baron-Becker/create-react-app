83b1ed4c9134781943aabcb051a6bc22310f72d6
BaronBecker <60110113+BaronBecker@users.noreply.github.com>
2023-06-01 09:46:30 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,240 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { usePrivy } from "@privy-io/react-auth";
+import Cookies from "js-cookie";
 
 function App() {
-  const { ready, authenticated, user, login, logout } = usePrivy();
-
+  const { ready, authenticated, user, login, logout, linkEmail, linkWallet, unlinkWallet, unlinkEmail, linkGoogle, unlinkGoogle, linkApple, unlinkApple, linkTwitter, unlinkTwitter, linkPhone, unlinkPhone} = usePrivy(); // Add linkEmail from usePrivy hook
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
   // Wait until the Privy client is ready before taking any actions
+  useEffect(() => {
+    if (ready) {
+      if (authenticated) {
+        Cookies.set("user", JSON.stringify(user));
+      } else {
+        Cookies.remove("user");
+      }
+    }
+  }, [ready, authenticated, user]);
+
   if (!ready) {
     return null;
   }
 
+  const handleLogout = () => {
+    logout();
+    setShowDropdown(false);
+  };
+  
+  const handleLinkEmail = () => {
+    linkEmail(); // Call linkEmail when the button is clicked
+  };
+
+  const handleunLinkEmail = (emailAddress) => {
+    unlinkEmail(emailAddress); // Call unlinkEmail with the email address when the button is clicked
+  };
+
+  const handleLinkWallet = () => {
+    linkWallet(); // Call linkEmail when the button is clicked
+  };
+
+  const handleunLinkWallet = (walletAddress) => {
+    unlinkWallet(walletAddress); // Call unlinkWallet with the wallet address when the button is clicked
+  };
+
+  const handleLinkGoogle = () => {
+    linkGoogle(); // Call unlinkWallet with the wallet address when the button is clicked
+  };
+
+  const handleunLinkGoogle = () => {
+    unlinkGoogle(); // Call unlinkWallet with the wallet address when the button is clicked
+  };
+
+  const handleLinkApple = () => {
+    linkApple(); // Call unlinkWallet with the wallet address when the button is clicked
+  };
+
+  const handleunLinkApple = () => {
+    unlinkApple(); // Call unlinkWallet with the wallet address when the button is clicked
+  };
+
+  const handleLinkTwitter = () => {
+    linkTwitter(); // Call unlinkWallet with the wallet address when the button is clicked
+  };
+
+  const handleunLinkTwitter = () => {
+    unlinkTwitter(); // Call unlinkWallet with the wallet address when the button is clicked
+  };
+
+  const handleLinkPhone = () => {
+    linkPhone(); // Call unlinkWallet with the wallet address when the button is clicked
+  };
+
+  const handleunLinkPhone = (number) => {
+    unlinkPhone(number); // Call unlinkWallet with the wallet address when the button is clicked
+  };
+
+
+  const toggleDropdown = () => {
+    setShowDropdown((prevState) => !prevState);
+    setButtonClicked((prevState) => !prevState);
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* If the user is not authenticated, show a login button */}
-        {/* If the user is authenticated, show the user object and a logout button */}
-        {ready && authenticated ? (
-          <div>
-            <textarea
-              readOnly
-              value={JSON.stringify(user, null, 2)}
-              style={{ width: "600px", height: "250px", borderRadius: "6px" }}
-            />
-            <br />
-            <button onClick={logout} style={{ marginTop: "20px", padding: "12px", backgroundColor: "#069478", color: "#FFF", border: "none", borderRadius: "6px" }}>
-              Log Out
-            </button>
-          </div>
-        ) : (
-          <button onClick={login} style={{padding: "12px", backgroundColor: "#069478", color: "#FFF", border: "none", borderRadius: "6px" }}>Log In</button>
-        )}
-      </header>
+    <div>
+      {authenticated ? (
+        <div className="dropdown">
+          <button className={`privy-login-button${buttonClicked ? ' clicked' : ''}`} onClick={toggleDropdown}>
+            {user.wallet
+              ? `${user.wallet.address.substring(0, 4)}...${user.wallet.address.slice(-5)}`
+              : (user.email
+                  ? user.email.address
+                  : (user.google
+                      ? user.google.email
+                      : (user.twitter
+                          ? user.twitter.username
+                          : (user.apple
+                              ? user.apple.email
+                              : (user.phone
+                                  ? user.phone.number
+                                  : 'None'
+                                )
+                            )
+                        )
+                    )
+                )
+            }
+          </button>
+
+          {showDropdown && (
+            <div className="dropdown-content">
+              <div className="privy-dropdown">
+                <h5 className="account-text">Account</h5>
+                <div className="createdat-text">created: {user && user.createdAt && user.createdAt.toLocaleDateString([], { year: 'numeric', month: 'long', day: 'numeric' })}</div>
+                <div className="cred-div">
+                  <div className="privy-dropdown-text">
+                    Wallet: {user.wallet ? `${user.wallet.address.substring(0, 4)}...${user.wallet.address.slice(-5)}` : "None"}
+                  </div>
+                  <button
+                    className="add-login-button w-button add-wallet"
+                    onClick={() => {
+                      if (user.wallet) {
+                        handleunLinkWallet(user.wallet.address);
+                      } else {
+                        handleLinkWallet();
+                      }
+                    }}
+                  >
+                    {user.wallet ? "Remove" : "Add"}
+                  </button>
+                </div>
+                <div className="cred-div">
+                  <div className="privy-dropdown-text">
+                    Email: {user.email ? user.email.address : "None"}
+                  </div>
+                  <button
+                    className="add-login-button w-button"
+                    onClick={() => {
+                      if (user.email) {
+                        handleunLinkEmail(user.email.address);
+                      } else {
+                        handleLinkEmail();
+                      }
+                    }}
+                  >
+                    {user.email ? "Remove" : "Add"}
+                  </button>
+                </div>
+                <div className="cred-div">
+                  <div className="privy-dropdown-text">
+                    Google: { user.google ? user.google.email : 'None' }
+                  </div>
+                  <button
+                    className="add-login-button w-button"
+                    onClick={() => {
+                      if (user.google) {
+                        handleunLinkGoogle(user.google.email);
+                      } else {
+                        handleLinkGoogle();
+                      }
+                    }}
+                  >
+                    {user.google ? "Remove" : "Add"}
+                  </button>
+                </div>
+                <div className="cred-div">
+                  <div className="privy-dropdown-text">
+                    Twitter: { user.twitter ? user.twitter.username : 'None' }
+                  </div>
+                  <button
+                    className="add-login-button w-button"
+                    onClick={() => {
+                      if (user.twitter) {
+                        handleunLinkTwitter(user.twitter.username);
+                      } else {
+                        handleLinkTwitter();
+                      }
+                    }}
+                  >
+                    {user.twitter ? "Remove" : "Add"}
+                  </button>
+                </div>
+                <div className="cred-div">
+                  <div className="privy-dropdown-text">
+                    Apple: { user.apple ? user.apple.email : 'None' }
+                  </div>
+                  <button
+                    className="add-login-button w-button"
+                    onClick={() => {
+                      if (user.apple) {
+                        handleunLinkApple(user.apple.email);
+                      } else {
+                        handleLinkApple();
+                      }
+                    }}
+                  >
+                    {user.apple ? "Remove" : "Add"}
+                  </button>
+                </div>
+                <div className="cred-div">
+                  <div className="privy-dropdown-text">
+                    Phone: { user.phone ? user.phone.number : 'None' }
+                  </div>
+                  <button
+                    className="add-login-button w-button"
+                    onClick={() => {
+                      if (user.phone) {
+                        handleunLinkPhone(user.phone.number);
+                      } else {
+                        handleLinkPhone();
+                      }
+                    }}
+                  >
+                    {user.phone ? "Remove" : "Add"}
+                  </button>
+                </div>
+                <div class="cred-div logout-button-privy">
+                  <div class="privy-dropdown-text" onClick={handleLogout}>
+                    Logout
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      ) : (
+        <button className="privy-login-button" onClick={login}>
+          Log In
+        </button>
+      )}
     </div>
   );
+  
+  
+  
+  
 }
 
 export default App;
